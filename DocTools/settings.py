dff76e4abe3a456e5eb69d06e0b459783f9f7d80
gundoprosper <gundotshili@gmail.com>
2021-01-28 01:09:43 +0200

    Changed db in cofig

--- DocTools/settings.py
+++ DocTools/settings.py
@@ -85,10 +85,21 @@ WSGI_APPLICATION = 'DocTools.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'master-postgres',
+        'PASSWORD': 'postgres1234',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
