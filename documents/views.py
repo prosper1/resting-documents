7c80cdb4741092632d7087ce9927dd5c00ff2319
gundoprosper <gundotshili@gmail.com>
2021-01-27 18:04:30 +0200

    Added Custom pagination to docs

--- documents/views.py
+++ documents/views.py
@@ -11,22 +11,26 @@ from rest_framework.authentication import (
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
+from rest_framework import permissions
+from .paginations import CustomPagination
 
 # Create your views here.
 
 class DocumentPagesView(viewsets.ModelViewSet):
-    # authentication_classes = [
-    #     BaseAuthentication,
-    #     TokenAuthentication,
-    #     SessionAuthentication
-    # ]
-    
+    authentication_classes = [
+        TokenAuthentication,
+        SessionAuthentication
+    ]
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = CustomPagination
     queryset = TextPages.objects.all()
     serializer_class = DocumentSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('document',)
     search_fields = ['document']
     http_method_names = ['get']
+
+    
     
 
     
